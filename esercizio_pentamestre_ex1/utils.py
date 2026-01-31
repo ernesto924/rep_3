357ea5a36d880d1be63ebffe009247914859c560
ernesto924 <efavoroso@gmail.com>
2026-01-31 09:06:28 +0000

    funzione sommamatrice

--- esercizio_pentamestre_ex1/utils.py
+++ esercizio_pentamestre_ex1/utils.py
@@ -1,4 +1,13 @@
-def stampa_matrice[A : list[list]]:
-for i in range (len(A)):
-    for j in range (len(A[i])):
-        print (A[i][j], end="\t")
+def stampa_matrice(A : list[list]):
+    for i in range (len(A)):
+        for j in range (len(A[i])):
+            print (A[i][j], end="\t")
+        print()
+
+
+def somma_matrice(A:list[list]):
+    somma=0
+    for i in range (len(A)):
+        for j in range (len(A[i])):
+                somma = somma + A[i][j]
+    return somma
