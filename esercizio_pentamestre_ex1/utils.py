8f09911552383fb56ae5aa78b1e56802ddc013d4
ernesto924 <efavoroso@gmail.com>
2026-02-02 12:19:16 +0000

    foglio 2 es_1 e es_2

--- esercizio_pentamestre_ex1/utils.py
+++ esercizio_pentamestre_ex1/utils.py
@@ -1,21 +1,34 @@
-def stampa_matrice(A : list[list]):
-    for i in range (len(A)):
-        for j in range (len(A[i])):
-            print (A[i][j], end="\t")
+import numpy as np
+
+
+def stampa_matrice(A: list[list]):
+    for i in range(len(A)):
+        for j in range(len(A[i])):
+            print(A[i][j], end="\t")
         print()
 
 
-def somma_matrice(A:list[list]):
-    somma=0
-    for i in range (len(A)):
-        for j in range (len(A[i])):
-                somma = somma + A[i][j]
+def somma_matrice(A: list[list]):
+    somma = 0
+    for i in range(len(A)):
+        for j in range(len(A[i])):
+            somma = somma + A[i][j]
     return somma
 
-def min_matrice(A:list[list]):
-    min=A[0][0]
-    for i in range (len(A)):
-        for j in range (len(A[i])):
-            if A[i][j]< min:
-                min=A[i][j]
+
+def min_matrice(A: list[list]):
+    min = A[0][0]
+    for i in range(len(A)):
+        for j in range(len(A[i])):
+            if A[i][j] < min:
+                min = A[i][j]
     return min
+
+
+def somma_elementi(A: np.ndarray, B: np.ndarray) -> np.ndarray:
+    """Somma due matrici"""
+    C = np.zeros_like(A)
+    for i in range(A.shape[0]):
+        for j in range(A.shape[1]):
+            C[i][j] = A[i][j] + B[i][j]
+    return C
